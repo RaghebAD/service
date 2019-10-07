6a6ba42be7eb3b2dd96cb6c628bccb358d2e0e44
raghebad <ragheb.abo.dargham@gmail.com>
2019-10-07 15:08:01 +0300

    added files

---
+++ config.js
@@ -0,0 +1,86 @@
+'use strict';
+
+module.exports = {
+    type: 'service',
+    prerequisites: {
+        cpu: '',
+        memory: ''
+    },
+    "serviceVersion": 1,
+    "serviceName": "example",
+    "serviceGroup": "SOAJS Core Services",
+    "servicePort": 4009,
+    "requestTimeout": 30,
+    "requestTimeoutRenewal": 5,
+    "extKeyRequired": true,
+    "oauth": true,
+    "maintenance": {
+        "commands": [
+            {"label": "Releoad Provision", "path": "/loadProvision", "icon": "provision"}
+        ]
+    },
+    "hashIterations": 1024,
+    "seedLength": 32,
+    "model": 'mongo',
+    "loginMode": "oauth",
+    "errors": {
+    },
+    "schema": {
+        "get": {
+            '/testGet': {
+                "_apiInfo": {
+                    "l": "test get",
+                    "group": "Read"
+                },
+            }
+        },
+        "post": {
+            '/testPost': {
+                "_apiInfo": {
+                    "l": "test post",
+                    "group": "Write"
+                },
+            }
+        },
+        "put": {
+            '/testPut': {
+                "_apiInfo": {
+                    "l": "test put",
+                    "group": "Update"
+                },
+            }
+        },
+        "delete": {
+            '/testDelete': {
+                "_apiInfo": {
+                    "l": "test delete",
+                    "group": "Delete"
+                },
+            }
+        },
+        "patch": {
+            '/testPatch': {
+                "_apiInfo": {
+                    "l": "test patch",
+                    "group": "Patch"
+                },
+            }
+        },
+        "head": {
+            '/testHead': {
+                "_apiInfo": {
+                    "l": "test head",
+                    "group": "Head"
+                },
+            }
+        },
+        // "test": {
+        //     '/testOther': {
+        //         "_apiInfo": {
+        //             "l": "test other",
+        //             "group": "Other"
+        //         },
+        //     }
+        // }
+    }
+};
